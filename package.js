e2567f4750eb8760b2b7967fc38d2050b75ba5e1
Cenk Dolek <cdolek@gmail.com>
2016-06-05 02:12:09 +0300

    package.js updated for dependencies

--- package.js
+++ package.js
@@ -1,25 +1,29 @@
 Package.describe({
-  name: 'cenk:accounts-password-strength',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: 'This package allows you to set password strength for your meteor application.',
-  // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/cdolek/accounts-password-strength/tree/master',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name: 'cenk:accounts-password-strength',
+    version: '0.0.1',
+    summary: 'This package allows you to set strong password strength for your meteor application. Also places a strength indicator.',
+    git: 'https://github.com/cdolek/accounts-password-strength/tree/master',
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.3.2.4');
-  api.use('ecmascript');
-  api.use('accounts-password');
-  api.mainModule('accounts-password-strength.js');
+    api.versionsFrom('1.3.2.4');
+    api.use('ecmascript');
+    api.use('useraccounts:bootstrap');
+    api.use('accounts-password');
+    api.use('check');
+    api.use(['templating'], 'client');
+    api.addFiles([
+        'atTextInputWithPasswordStrength.css',
+        'atTextInputWithPasswordStrength.html',
+        'atTextInputWithPasswordStrength.js'
+    ], 'client');
+    api.mainModule('accounts-password-strength.js');
 });
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('cenk:accounts-password-strength');
-  api.mainModule('accounts-password-strength-tests.js');
+    api.use('ecmascript');
+    api.use('tinytest');
+    api.use('cenk:accounts-password-strength');
+    api.mainModule('accounts-password-strength-tests.js');
 });
