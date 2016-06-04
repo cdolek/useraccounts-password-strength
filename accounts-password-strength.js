35ef4f8f6e2b631ca2f0c57e10a7f18220e80a7d
Cenk Dolek <cdolek@gmail.com>
2016-06-05 02:12:50 +0300

    main fns

--- accounts-password-strength.js
+++ accounts-password-strength.js
@@ -1,5 +1,74 @@
-// Write your package code here!
+import {
+    AccountsTemplates
+} from 'meteor/useraccounts:core';
 
-// Variables exported by this module can be imported by other packages and
-// applications. See accounts-password-strength-tests.js for an example of importing.
-export const name = 'accounts-password-strength';
+import {
+    Template
+} from 'meteor/templating';
+
+AccountsTemplates.removeField('password');
+
+AccountsTemplates.addField({
+    _id: 'password',
+    type: 'password',
+    minLength: 8,
+    required: true,
+    func: function(value) {
+        var self = this;
+        if (Meteor.isClient) {
+            var result = getPasswordStrength(value);
+            if (result.strength !== 4) {
+                return true; // true means NOT valid
+            } else {
+                self.setSuccess();
+            }
+        }
+    },
+    errStr: 'Does not meet the requirements',
+    template: 'atTextInputWithPasswordStrength'
+});
+
+Accounts.passwordStrength = {
+    rules: {
+        upperCase: true,
+        lowerCase: true,
+        digits: true,
+        specialCharacters: true
+    },
+    strengthTextArr: [
+        "",
+        "Low",
+        "Medium",
+        "High",
+        "Strong"
+    ]
+};
+
+
+export function getPasswordStrength(password) {
+
+    check(password, String);
+
+    var strength = 0;
+
+    if (password.trim().length !== 0) {
+        // Regular Expressions
+        var regex = new Array();
+        regex.push("[A-Z]"); // Uppercase Alphabet.
+        regex.push("[a-z]"); // Lowercase Alphabet.
+        regex.push("[0-9]"); // Digit.
+        regex.push("[$@$!%*#?&]"); // Special Character.
+
+        // Validate for each Regular Expression
+        for (var i = 0; i < regex.length; i++) {
+            if (new RegExp(regex[i]).test(password)) {
+                strength++;
+            }
+        }
+    }
+    return {
+        strength: strength,
+        strengthText: strength ? "Strength: " + Accounts.passwordStrength.strengthTextArr[strength] : '',
+        isAtRequiredStrength: strength === 4
+    };
+}
