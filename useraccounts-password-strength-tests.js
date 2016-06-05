18de921715dd9d7611f21f0b64fc0d662493fff4
Cenk Dolek <cdolek@gmail.com>
2016-06-05 03:52:48 +0300

    test init

--- useraccounts-password-strength-tests.js
+++ useraccounts-password-strength-tests.js
@@ -1,9 +1,6 @@
 // Import Tinytest from the tinytest Meteor package.
 import { Tinytest } from "meteor/tinytest";
 
-// Import and rename a variable exported by useraccounts-password-strength.js.
-import { name as packageName } from "meteor/useraccounts-password-strength";
-
 Tinytest.add('useraccounts-password-strength - example', function (test) {
-  // test.equal(packageName, "useraccounts-password-strength");
+  test.equal(1, 1);
 });
